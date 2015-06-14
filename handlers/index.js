f8743b719f94e69561e194b75b2409d45b137403
Kshitij Gautam <kshitijgautam@live.com>
2015-06-14 01:57:38 -0500

    testing branch in git

--- handlers/index.js
+++ handlers/index.js
@@ -45,7 +45,7 @@ for (var number in flight_data) {
 Flight() is a function defined in ./flight_module
 */
 exports.greetings = function(req, res) {
-	res.send("hello");
+	res.send("hello world from another branch");
 };
 exports.flight1 = function(req, res) { //we have to call this function using app.js
 	res.json(flight1.getInformation());
